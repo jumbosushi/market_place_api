cd1a808f30ffe0fbb7714ce79a5fe01e5b572d18
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-09-02 00:23:35 -0700

    Fix rspec errors

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -1,12 +1,11 @@
 class ApiConstraints
 
   def initialize(options)
-    @version = option[:version]
-    @default = option[:default]
+    @version = options[:version]
+    @default = options[:default]
   end
 
-  def matched?(req)
+  def matches?(req)
     @default || req.headers['Accept'].include?("application/vnd.marketplace.v#{@version}")
   end
-
 end
